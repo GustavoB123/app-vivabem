7f92813d5881a4269b7582652a95e36ecfaf4bb8
Gustavo Brito <ggustavobrito124@gmail.com>
2024-04-30 17:23:19 -0300

    Update: Criação da pag de login e dashboard, ligação entre elas já feita

--- App.js
+++ App.js
@@ -1,51 +1,73 @@
 import * as React from 'react';
-import { Button, View, Image, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import { Button, View, Image, Text, TextInput, estiloheet, TouchableOpacity, ImageBackground } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { estilo }  from './estilo/estilo'
+
+const ImgBack = './assets/fundo.png'
 
 
 // HOME SCREEN
 
 const CustomButton = ({ onPress, title, buttonStyle, textStyle }) => {
   return (
-    <TouchableOpacity onPress={onPress} style={[styles.button, buttonStyle]}>
-      <Text style={[styles.text, textStyle]}>{title}</Text>
+    <TouchableOpacity onPress={onPress} style={[estilo.button, buttonStyle]}>
+      <Text style={[estilo.text, textStyle]}>{title}</Text>
     </TouchableOpacity>
   );
 };
 
-// const styles = StyleSheet.create({
-//   button: {
-    
-//   },
-//   text: {
-//     color: 'white',
-//     fontSize: 16,
-//   },
-// });
+
 
 function LoginScreen({ navigation }) {
   return (
+    
+
     <View style={{flex: 1, alignItems: 'center',  backgroundColor: '# ',}}>
-      <Image style={styles.img} source={require('./assets/logoVivaBem.svg')} />
-      <Text style={styles.texto}>LOGIN</Text>
-      <TextInput style={styles.input} placeholder='Email' />
-      <TextInput secureTextEntry={true} style={styles.input} placeholder='Senha' />
-      <CustomButton title="Entrar" onPress={() => navigation.navigate('Profile')} style={styles.btn}/>
-      <view>
-      </view>   
+      <ImageBackground source={require(ImgBack)} style={{width: '100%', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Image style={estilo.img} source={require('./assets/logoVivaBem.svg')} />
+      <TextInput style={estilo.input} placeholder='Email' />
+      <TextInput secureTextEntry={true} style={estilo.input} placeholder='Senha' />
+      <CustomButton title="Entrar" onPress={() => navigation.navigate('Dashboard')} buttonStyle={estilo.btn} textStyle={estilo.btnTexto} />
+      <CustomButton title="Esqueci a senha" onPress={() => navigation.navigate('Settings')} buttonStyle={estilo.btn2} textStyle={estilo.btn2Texto} />
+      </ImageBackground>
     </View>
   );
 }
 
-
-
-
-function ProfileScreen({ navigation }) {
+function DashboardScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button title="Go to Notifications" onPress={() => navigation.navigate('Notifications')} style={styles.btn}/>
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+    <View style={estilo.dashView}>
+      
+      <Text style={{fontFamily: 'Segoe UI', fontSize: 40, fontWeight: '700', color: '#34495e', marginTop: 50,}}>Olá, usuário</Text>
+      <Text style={{fontFamily: 'Segoe UI', fontSize: 25, fontWeight: '400', color: '#707070', marginTop: 30}}> Nome do Plano</Text>
+
+      <view style={estilo.dashB1}>
+        <Text style={{fontFamily: 'Segoe UI', fontSize: 25, fontWeight: '400', color: '#707070'}}>Você está na sua</Text>
+        <Text style={{fontFamily: 'Segoe UI', fontSize: 40, fontWeight: '700', color: '#34495e'}}>contª</Text>
+        <Text style={{fontFamily: 'Segoe UI', fontSize: 25, fontWeight: '400', color: '#707070'}}>sessão de treinos</Text>
+      </view>
+
+      <view style={estilo.dashB2}>
+        <CustomButton title='Acesse suas aulas' buttonStyle={estilo.btnDash} textStyle={estilo.TextoBtnDash}/>
+        <CustomButton title='Acesse seus treinos' buttonStyle={estilo.btnDash} textStyle={estilo.TextoBtnDash}/>
+      </view>
+
+
+      <Image source={require('./assets/treinoDash.png')} style={{width: 345, height: 219, marginTop: 30}} />
+
+      <CustomButton title='UPGRADE NO PLANO' buttonStyle={estilo.btnPlano} textStyle={estilo.btnTPlano}/>
+
+
+      <view style={estilo.dashFooter}>
+        <view style={estilo.dashIcon}>
+          <Image source={require('./assets/icons/iconExe.svg')} style={{width: 40, height: 40, borderRadius: 50 }}/>
+          <Text>Exercícios</Text>
+          
+        </view>
+      </view>
+
+
     </View>
   );
 }
@@ -74,10 +96,10 @@ const Stack = createStackNavigator();
 
 function MyStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerShown : false}}>
       <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Dashboard" component={DashboardScreen} />
       <Stack.Screen name="Notifications" component={NotificationsScreen} />
-      <Stack.Screen name="Profile" component={ProfileScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
   );
@@ -91,37 +113,5 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
 
-    img: {
-      width: 320,
-       height: 100,
-        marginTop: 75
-    },
-
-    texto: {
-      color: '#23475F', 
-      fontFamily: 'Segoe UI', 
-      fontSize: 35, 
-      marginTop: 50,
-      fontWeight: '500',
-    },
-
-    input: {
-      fontFamily: 'Segoe UI',
-      fontSize: 23,
-      borderBottomWidth: 1,
-      borderColor: '#00CAB0',
-      marginTop: 40
-    },
-
-    btn: {
-      backgroundColor: 'blue',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    },
-});
 
---
+++ estilo/estilo.js
@@ -0,0 +1,141 @@
+export const estilo = {
+    img: {
+        width: 320,
+         height: 100,
+          marginTop: 75
+      },
+  
+      texto: {
+        color: '#23475F', 
+        fontFamily: 'Segoe UI', 
+        fontSize: 35, 
+        marginTop: 50,
+        fontWeight: '500',
+      },
+  
+      input: {
+        fontFamily: 'Segoe UI',
+        fontSize: 23,
+        borderBottomWidth: 1,
+        borderColor: '#00CAB0',
+        marginTop: 40,
+        
+      },
+  
+      btn: {
+      backgroundColor: '#34495e',
+      borderStyle: "none",
+      width: 200,
+      height: 45,  
+      borderRadius: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: 35,
+      },
+  
+      btnTexto: {
+        color: 'white',
+        fontSize: 18,
+        fontFamily: 'Segoe UI',
+        fontWeight: '600',
+      },
+  
+      btn2: {
+        backgroundColor: '#48c9b0',
+        borderStyle: "none",
+        width: 175,
+        height: 35,
+        color: 'white',
+        borderRadius: 15,
+        fontSize: 15,
+        fontWeight: '600',
+        marginTop: 25,
+        justifyContent: 'center',
+      },
+  
+      btn2Texto: {
+        fontSize: 15,
+        fontWeight: '600',
+        color: 'white',
+        alignSelf: 'center',
+      },  
+    // --------
+    
+    
+    //Dashboard
+  
+      dashView: {
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'flex-start'
+      },  
+  
+      dashB1: {
+        display: 'flex', 
+        alignItems: 'center', 
+        borderStyle: 'solid', 
+        borderWidth: 2, 
+        borderColor: '#707070', 
+        flexDirection: 'column', 
+        height: 200, 
+        width: 360, 
+        justifyContent: 'center', marginTop: 30
+      },
+      
+      dashB2: {
+        display: 'flex',
+        gap: 20,
+      },
+  
+      btnDash: {
+        width: 170,
+        height: 40,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#48c9b0',
+        marginTop: 10,
+      },
+  
+      btnPlano: {
+        width: 200,
+        height: 40,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFC500',
+        marginTop: 49,
+      },
+  
+      btnTPlano: {
+        fontFamily: 'Segoe UI',
+        fontSize: 17,
+        fontWeight: '700',
+        color: 'white',
+      },
+  
+      TextoBtnDash: {
+        fontFamily: 'Segoe UI',
+        fontSize: 16,
+        fontWeight: '700',
+        color: 'white',
+      },
+
+
+      // Footer Dashboard
+
+      dashFooter: {
+        width: 768,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+      },
+
+      dashIcon: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }
+}
